f0edf3f3c82b8817cff6bb5e1151134c8fefeffc
Luke D Buchanan <luke.d.buchanan@pwc.com>
2022-01-26 10:53:39 +0000

    First commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class GradeBookMain
+{
+    static void Main(string[] args)
+    {
+        var numbers = new[] {12.7, 10.3, 6.11};
+
+        var result = 0.0;
+
+        foreach(double number in numbers)
+        {
+            result += number;
+        }
+
+        // Display the number of command line arguments.
+        if(args.Length > 0)
+        {
+        Console.WriteLine($"Hello, {args[1]}!");
+        }
+        else
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+        System.Console.WriteLine(result);
+    }
+}
